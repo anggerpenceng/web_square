b8c5326eae9d93536f3311137412e14880ecd802
Angger PW <anggerpenceng10cm@gmail.com>
2020-04-21 21:12:30 +0700

    handle 3 page service and testi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 $(function () {
     $(document).scroll(function () {
       var $nav = $("#nav-fix");
-      var $navItem = $(".navigation-items");
+      var $navItem = $(".in-navs");
       $nav.toggleClass('nav-scroll', $(this).scrollTop() > $nav.height());
       $navItem.toggleClass('nav-item-scroll', $(this).scrollTop() > $nav.height());
     });
-  });
+});
