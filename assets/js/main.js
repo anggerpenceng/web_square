533025a56f700531b047586df02cb967b48b3c23
Angger PW <anggerpenceng10cm@gmail.com>
2020-04-25 21:32:13 +0700

    fixing pixel complex

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,11 @@ $(function () {
     $(document).scroll(function () {
       var $nav = $("#nav-fix");
       var $navItem = $(".in-navs");
+      var $navIco = $(".icon");
+
+
       $nav.toggleClass('nav-scroll', $(this).scrollTop() > $nav.height());
       $navItem.toggleClass('nav-item-scroll', $(this).scrollTop() > $nav.height());
+      $navIco.toggleClass('nav-img-scroll', $(this).scrollTop() > $nav.height());
     });
 });
