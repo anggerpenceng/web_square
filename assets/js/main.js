cc92f078f83abf891013e6eb364f324f8aea780a
Angger PW <anggerpenceng10cm@gmail.com>
2020-05-12 22:40:21 +0700

    fixing revise project

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,20 @@ $(function () {
       var $nav = $("#nav-fix");
       var $navItem = $(".in-navs");
       var $navIco = $(".icon");
+      var $homeIco = $(".home-ico");
+      var $homeIcoBlack = $("#home-black");
 
 
       $nav.toggleClass('nav-scroll', $(this).scrollTop() > $nav.height());
       $navItem.toggleClass('nav-item-scroll', $(this).scrollTop() > $nav.height());
       $navIco.toggleClass('nav-img-scroll', $(this).scrollTop() > $nav.height());
+      $homeIco.toggleClass('home-ico-hide', $(this).scrollTop() > $nav.height());
+
+      if ($(this).scrollTop() > $nav.height())
+      {
+        $homeIcoBlack.removeClass('home-ico-hide')
+      }else{
+        $homeIcoBlack.addClass('home-ico-hide')
+      }
     });
 });
